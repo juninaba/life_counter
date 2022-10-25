0466a3efce988deaf08d9e1de5a7efc5da87a16c
juninaba <43479094+juninaba@users.noreply.github.com>
2022-10-25 17:46:04 +0900

    first commit

---
+++ lib/life_event.dart
@@ -0,0 +1,13 @@
+import 'package:objectbox/objectbox.dart';
+
+@Entity()
+class LifeEvent {
+  LifeEvent({
+    required this.title,
+    required this.count,
+  });
+
+  int id = 0;
+  String title;
+  int count;
+}
